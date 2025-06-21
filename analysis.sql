60282549c7cc473df4a48881358ce4583d0a153b
Alfredo <alfredo@Alfredos-MacBook-Pro.local>
2025-06-21 14:38:06 +0700

    Added Frequency and Monetary segmentation(6)

--- analysis.sql
+++ analysis.sql
@@ -1,9 +1,8 @@
--- Reference Date
+-- RFM ANALYSIS WITH 6 SEGMENTATION --
+-- Recency
 WITH reference_date AS (
 SELECT MAX(order_date) AS ref_date FROM e_commerce_transactions 
 ),
-
--- Recency
 customer_recency AS (
 SELECT
 customer_id,
@@ -11,10 +10,40 @@ customer_id,
 FROM e_commerce_transactions
 GROUP BY customer_id
 )
--- Recency Segment
 SELECT *,
 NTILE(6) OVER (ORDER BY recency_day ASC) AS recency_segment
 FROM customer_recency
 ORDER BY recency_day;
 
 -- Frequency
+WITH customer_frequency AS (
+SELECT
+customer_id,
+COUNT(order_id) AS frequency
+FROM e_commerce_transactions
+GROUP BY customer_id
+),
+scored_frequency AS (
+SELECT *,
+NTILE(6) OVER (ORDER BY frequency) AS frequency_segment
+FROM customer_frequency
+)
+SELECT * FROM scored_frequency;
+
+-- Monetary
+With customer_monetary AS (
+SELECT
+customer_id,
+SUM(payment_value) AS monetary_value
+FROM e_commerce_transactions
+GROUP BY customer_id
+),
+monetary_segmented AS (
+SELECT 
+customer_id,
+monetary_value,
+NTILE(6) OVER (ORDER BY monetary_value) AS monetary_segment
+FROM customer_monetary 
+)
+SELECT * FROM monetary_segmented
+ORDER BY monetary_segment;
