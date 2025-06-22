3b61d099c8f3ada9a33363953ee0a9b0cfc58fe8
Alfredo <alfredo@Alfredos-MacBook-Pro.local>
2025-06-22 15:23:04 +0700

    Added cutoff summary for expected default of <= 5%

--- validation.R
+++ validation.R
@@ -49,7 +49,6 @@ cal_plot <- ggplot(calibration_data, aes(x = avg_pred, y = obs_rate)) +
     )
 
 ggsave("~/Downloads/calibration_curve.png", plot = cal_plot, width = 6, height = 4, dpi = 300)
-
 ######################################################################
 # The calibration curve shows that the predicted probabilities align 
 # with the actual default rates reasonably, especially in the higher
@@ -59,3 +58,25 @@ ggsave("~/Downloads/calibration_curve.png", plot = cal_plot, width = 6, height =
 ######################################################################
 
 
+thresholds <- seq(0.01, 1, by = 0.01)
+cutoff <- NA
+
+for (t in thresholds) {
+    group <- df %>% filter(predicted >= t)
+    if (nrow(group) > 0) {
+        rate <- mean(group$actual)
+        if (rate <= 0.05) {
+            cutoff <- t
+            break
+        }
+    }
+}
+
+print(paste("Cut-off score with expected default <= 5%:", cutoff))
+######################################################################
+# The cut-off score is extremely low at 0.01 - it implies that the 
+# model are very conservative. Recall through our dataset, the imbalance
+# case are only 1.23% defaulters in total and most features are uniformly
+# distributed. The model learned that most people don't default, so it 
+# outputs low scores for nearly everyone.
+######################################################################
